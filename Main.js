51badaeba4619515bad0130f58f5c478fa555fe8
112swde <82498343+112swde@users.noreply.github.com>
2021-10-07 11:29:01 +0530

    Add files via upload

--- Main.js
+++ Main.js
@@ -1,12 +1,28 @@
+prediction_1 = ""
+
+Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+  });
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+
 function  take_snapshot()
 {
     Webcam.snap(function(data_uri) {
-        document.getElementById("camera").innerHTML = "<img id='captured_image' src="+data_uri+"/>";
+        document.getElementById("camera").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
     });
 }
 
 console.log("ml5 version :", ml5.version);
+
 classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/hNsRV1k5-/model.json",modelLoaded);
+
 function modelLoaded()
 {
     console.log("model loaded !")
@@ -15,8 +31,40 @@ function modelLoaded()
 function speak()
 {
     var synth = window.speechSynthesis;
-    speak_data1 = "The first prediction is" + prediction1;
-    speak_data2 = "and the second prediction is" + prediction2;
-    var utterThis = new SpeechSynthesisUtterance(speak_data1 + speak_data1);
+    speak_data1 = "The prediction is" + prediction_1;
+    var utterThis = new SpeechSynthesisUtterance(speak_data1);
     synth.speak(utterThis);
+}
+
+function check()
+{
+    img = document.getElementById("captured_image");
+    classifier.classify(img,gotResults);
+}
+
+function gotResults(error,results)
+{
+     if(error)
+     {
+         console.log(error);
+     }
+     else
+     {
+         console.log(results);
+         document.getElementById("result_gesture_name").innerHTML = results[0].label;
+         prediction_1 = results[0].label;
+         speak();
+         if (results[0].label == "great")
+         {
+             document.getElementById("update_gesture").innerHTML = "&#128077;";
+         }
+         if (results[0].label == "amazing")
+         {
+             document.getElementById("update_gesture").innerHTML = "&#128076;";
+         }
+         if (results[0].label == "victory")
+         {
+             document.getElementById("update_gesture").innerHTML = "&#9996;";
+         }
+     }
 }
